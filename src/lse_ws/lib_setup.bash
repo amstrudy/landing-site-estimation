21d7ec03884349fdd3d15138d571385497e41ae5
ForrestHurley <forrestelihurley0@gmail.com>
2018-03-22 11:40:54 -0400

    Made the setup.bash file not have absolute links that are likely to change from system to system

--- src/lse_ws/lib_setup.bash
+++ src/lse_ws/lib_setup.bash
@@ -6,8 +6,8 @@ source /opt/ros/kinetic/setup.bash
 
 # Load pkg
 
-source /home/anjasheppard/landing-site-estimation/src/lse_ws/devel/setup.bash
+source devel/setup.bash
 
 # Load Yocto Libraries
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/anjasheppard/landing-site-estimation/src/lse_ws/src/altimeter_pub/include/yocto/64bits
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:src/altimeter_pub/include/yocto/64bits
