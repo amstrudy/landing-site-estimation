5007f0bf10d96c18f7a458ce22f5695ef22eb85c
ForrestHurley <forrestelihurley0@gmail.com>
2018-04-05 00:12:41 -0400

    Deletions

--- src/situ.h
+++ src/situ.h
@@ -9,26 +9,65 @@ An addition to the Cambridge Rocketry Simulator to support predictions during a 
 #ifndef SITU_H
 #define SITU_H
 
-#include "RocketFlight.h"
-#include "ascentcalc.h"
-#include "descentcalc.h"
-#include "MonteFy.h"
-#include <chrono>
+//#include "RocketFlight.h"
+//#include "ascentcalc.h"
+//#include "descentcalc.h"
+//#include "MonteFy.h"
+//#include <chrono>
+#include <Eigen.h>
 
-class Situ : public Rocket_Flight{
-        int max_iters = 1000;
-        int getCurrentCondition();//0: ascent, 1: free-fall, 2: parachute, 3: landing
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
 
-        vector3 get_x(); //returns x, y, z position
-        vector3 get_v(); //returns x, y, z velocity
-        quaternion get_quat(); //returns heading of the rocket
-        vector3 get_ang_vel(); //returns angular velocity of the rocket
-    public:
+struct ballistic {
+private:
+	vector<double> times;
+	vector<vector3> positions;
+public:
+	void add_point(vector3 position, double time){
+		positions.append(position);
+		times.append(time)
+	}
+
+	vector3 getPosition(int idx) { return positions[idx]; };
+	double getTime(int idx) { return times[idx]; };
+
+	int size() { return positions.size(); };
+}
+
+class Situ{
+private:
+	int max_iters = 1000;
+
+	ballistic history;
+
+	//bool newState = false;
+	
+	double currentTime = 0;
+	vector3 position = vector3(0,0,0);
+	vector3 velocity = vector3(0,0,0);
+	float exponential_smoothing = 0.9;
+
+	const vector3 gravity = vector3(0,0,-9.8);
+	float delta_sim_time = 0.1;
+
+	vector3 calc_sim_drag() { return vector3(0,0,0); };
+    //int getCurrentCondition();//0: ascent, 1: free-fall, 2: parachute, 3: landing
+
+	//vector3 x; //returns x, y, z position
+	//vector3 v; //returns x, y, z velocity
+	//quaternion heading(); //returns heading of the rocket
+	//vector3 get_ang_vel(); //returns angular velocity of the rocket
+public:
        
-        vector<double> getCurrentTime();
-        vector<double> getCurrentState();
+	//double getCurrentTime();
+	//vector<double> getCurrentState();
 
-        vector<RKF_data> SituCalc(bool rand, MonteFy StochTab);
-        OutputData SituMonte(float runtime);
+	//vector<RKF_data> SituCalc(bool rand, MonteFy StochTab);
+	//OutputData SituMonte(float runtime);
+
+	void updateState(cons sensor_msgs::NavSatFix::ConstPtr& msg);
+	vector3 predictArc();
 };
 
